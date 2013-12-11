51b8f82c0a05c03f20e16d15f921405c6df6a63a
Joachim Metz <joachim.metz@gmail.com>
2013-12-11 09:44:59 +0100

    Code review: 39900043: Initial VHD support.

---
+++ pyvfs/path/vhdi_path_spec.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 The PyVFS Project Authors.
+# Please see the AUTHORS file for details on individual authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The VHD image path specification implementation."""
+
+from pyvfs.path import path_spec
+
+
+class VhdiPathSpec(path_spec.PathSpec):
+  """Class that implements the VHD image path specification."""
+
+  def __init__(self, parent):
+    """Initializes the path specification object.
+
+       Note that the vhdi file path specification must have a parent.
+
+    Args:
+      parent: parent path specification (instance of PathSpec).
+
+    Raises:
+      ValueError: when parent is not set.
+    """
+    if not parent:
+      raise ValueError(u'Missing parent value.')
+
+    super(VhdiPathSpec, self).__init__(parent=parent)
+
+  @property
+  def comparable(self):
+    """Comparable representation of the path specification."""
+    return self._GetComparable(u'')
