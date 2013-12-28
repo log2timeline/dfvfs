fcbba4905f5ed25e384ec901dd0dce39b0ecfe4a
Joachim Metz <joachim.metz@gmail.com>
2013-12-28 04:38:56 +0100

    Code review: 45720043:  Moved TSK volume to FS-based implementation.

--- pyvfs/volume/tsk_volume_system_test.py
+++ pyvfs/volume/tsk_volume_system_test.py
@@ -18,9 +18,10 @@
 """Tests for the volume system implementation using the SleuthKit (TSK)."""
 
 import os
-import pytsk3
 import unittest
 
+from pyvfs.path import os_path_spec
+from pyvfs.path import tsk_partition_path_spec
 from pyvfs.volume import tsk_volume_system
 
 
@@ -29,28 +30,32 @@ class TSKVolumeSystemTest(unittest.TestCase):
 
   def setUp(self):
     """Sets up the needed objects used throughout the test."""
-    self._test_file = os.path.join('test_data', 'tsk_volume_system.raw')
-    self._tsk_img = pytsk3.Img_Info(self._test_file)
+    test_file = os.path.join('test_data', 'tsk_volume_system.raw')
+
+    path_spec = os_path_spec.OSPathSpec(location=test_file)
+    self._tsk_path_spec = tsk_partition_path_spec.TSKPartitionPathSpec(
+        location=u'/', parent=path_spec)
+
+  # mmls test_data/tsk_volume_system.raw
+  # DOS Partition Table
+  # Offset Sector: 0
+  # Units are in 512-byte sectors
+  #
+  #      Slot    Start        End          Length       Description
+  # 00:  Meta    0000000000   0000000000   0000000001   Primary Table (#0)
+  # 01:  -----   0000000000   0000000000   0000000001   Unallocated
+  # 02:  00:00   0000000001   0000000350   0000000350   Linux (0x83)
+  # 03:  Meta    0000000351   0000002879   0000002529   DOS Extended (0x05)
+  # 04:  Meta    0000000351   0000000351   0000000001   Extended Table (#1)
+  # 05:  -----   0000000351   0000000351   0000000001   Unallocated
+  # 06:  01:00   0000000352   0000002879   0000002528   Linux (0x83)
 
   def testIterateVolumes(self):
     """Test the iterate volumes functionality."""
-    volume_system = tsk_volume_system.TSKVolumeSystem(self._tsk_img)
+    volume_system = tsk_volume_system.TSKVolumeSystem()
+    volume_system.Open(self._tsk_path_spec)
 
-    # mmls test_data/tsk_volume_system.raw
-    # DOS Partition Table
-    # Offset Sector: 0
-    # Units are in 512-byte sectors
-    #
-    #      Slot    Start        End          Length       Description
-    # 00:  Meta    0000000000   0000000000   0000000001   Primary Table (#0)
-    # 01:  -----   0000000000   0000000000   0000000001   Unallocated
-    # 02:  00:00   0000000001   0000000350   0000000350   Linux (0x83)
-    # 03:  Meta    0000000351   0000002879   0000002529   DOS Extended (0x05)
-    # 04:  Meta    0000000351   0000000351   0000000001   Extended Table (#1)
-    # 05:  -----   0000000351   0000000351   0000000001   Unallocated
-    # 06:  01:00   0000000352   0000002879   0000002528   Linux (0x83)
-
-    self.assertEquals(volume_system.block_size, 512)
+    self.assertEquals(volume_system.bytes_per_sector, 512)
 
     self.assertEquals(volume_system.number_of_sections, 7)
     self.assertEquals(volume_system.number_of_volumes, 2)
