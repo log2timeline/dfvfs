56a9ac58b639aebdb1bdeb0e8997e761594fcacf
Joachim Metz <joachim.metz@gmail.com>
2014-01-03 18:46:47 +0100

    Initial version of git cl support added to the utility scripts.
    R=kiddi@kiddaland.net
    
    Review URL: https://codereview.appspot.com/47470043

--- utils/common.sh
+++ utils/common.sh
@@ -19,7 +19,92 @@
 EXIT_FAILURE=1;
 EXIT_SUCCESS=0;
 
-linter()
+FALSE=1;
+TRUE=0;
+
+# Function to check for double status codes rietveld does not seem to handle
+# these correctly.
+have_double_git_status_codes()
+{
+  STATUS_CODES=`git status -s | cut -b1,2 | grep '\S\S' | grep -v '??' | sort | uniq`;
+
+  if ! test -z "${STATUS_CODES}";
+  then
+    return ${TRUE};
+  fi
+  return ${FALSE};
+}
+
+# Function to check if we have "git cl" or that we should use upload.py instead.
+have_git_cl()
+{
+  DUMMY=`git cl 2> /dev/null`;
+
+  if test $? -eq 0;
+  then
+    return ${TRUE};
+  fi
+  return ${FALSE};
+}
+
+# Function to check if we are on the master branch.
+have_master_branch()
+{
+  BRANCH="";
+  get_current_branch "BRANCH";
+
+  if test "${BRANCH}" = "master";
+  then
+    return ${TRUE};
+  fi
+  return ${FALSE};
+}
+
+# Function to check if we have uncommitted changes.
+have_uncommitted_changes()
+{
+  # Ignore the '??' status code.
+  GIT_STATUS=`git status -s | grep -v -e '^?? '`;
+
+  if test -z "${GIT_STATUS}";
+  then
+    return ${FALSE};
+  fi
+  return ${TRUE};
+}
+
+# Function to check if we have uncommitted changes with new files.
+have_uncommitted_changes_with_append_status()
+{
+  RESULT=${FALSE};
+  STATUS_CODES=`git status -s | cut -b1,2 | sed 's/\s//g' | sort | uniq`;
+
+  for STATUS_CODE in ${STATUS_CODES};
+  do
+    if test "${STATUS_CODE}" = "A";
+    then
+      RESULT=${TRUE};
+    fi
+  done
+  return ${RESULT};
+}
+
+# Function to retrieve the name of the current branch.
+get_current_branch()
+{
+  RESULT=`git branch | grep -e "^[*]" | sed "s/^[*] //"`;
+  eval "$1='${RESULT}'";
+}
+
+# Function to retrieve the desciption of the last committed change.
+get_last_change_description()
+{
+  RESULT=`git log | head -n5 | tail -n1 | sed 's/^[ ]*//'`;
+  eval "$1='${RESULT}'";
+}
+
+# Function to check if the linting of the changes is correct.
+linting_is_correct()
 {
   # Examples of the output of "git status -s"
   # If a file is added:
@@ -37,7 +122,7 @@ linter()
 
   LINTER="pylint --rcfile=utils/pylintrc"
 
-  echo "Run through pylint.";
+  echo "Running changes by pylint.";
 
   for FILE in ${FILES};
   do
@@ -59,16 +144,63 @@ linter()
     if test $? -ne 0;
     then
       echo "Fix linter errors before proceeding."
-      return ${EXIT_FAILURE};
+      return ${FALSE};
     fi
   done
 
   if test $? -ne 0;
   then
-    return ${EXIT_FAILURE};
+    return ${FALSE};
   fi
 
   echo "Linter clear.";
 
-  return ${EXIT_SUCCESS};
+  return ${TRUE};
+}
+
+# Function to check if the local repo is in sync with the origin.
+local_repo_in_sync_with_origin()
+{
+  git fetch
+
+  if test $? -ne 0;
+  then
+    echo "Unable to fetch updates from origin.";
+
+    exit ${EXIT_FAILURE};
+  fi
+  NUMBER_OF_CHANGES=`git log HEAD..origin/master --oneline | wc -l`;
+
+  if test $? -ne 0;
+  then
+    echo "Unable to determine if local repo is in sync with origin.";
+
+    exit ${EXIT_FAILURE};
+  fi
+
+  if test ${NUMBER_OF_CHANGES} -eq 0;
+  then
+    return ${TRUE};
+  fi
+  return ${FALSE};
+}
+
+# Function to check if the tests pass.
+# Note that the function will also return true (0) if the run_tests.py script
+# cannot be found.
+tests_pass()
+{
+  if ! test -e run_tests.py;
+  then
+    return ${TRUE};
+  fi
+
+  echo "Running tests.";
+  python run_tests.py
+
+  if test $? -eq 0;
+  then
+    return ${TRUE};
+  fi
+  return ${FALSE};
 }
