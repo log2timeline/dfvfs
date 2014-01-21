77331813ee17ae6ec9bb8e5fa938ad35d486c6f8
Joachim Metz <joachim.metz@gmail.com>
2014-01-21 20:41:44 +0100

    Code review: 54580043: Small change to update version script.

--- utils/update_version.sh
+++ utils/update_version.sh
@@ -21,5 +21,5 @@ DATE_DPKG=`date -R`;
 EMAIL_DPKG="Log2Timeline <log2timeline-dev@googlegroups.com>";
 
 sed -i -e "s/^\(__version__ = \)'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'$/\1'${DATE_VERSION}'/" dfvfs/__init__.py
-sed -i -e "s/^\(python-dfvfs \)([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-1)/\1(${DATE_VERSION}-1)/" config/dpkg/debian/changelog
-sed -i -e "s/^\( -- ${EMAIL_DPKG}  \).*$/\1${DATE_DPKG}/" config/dpkg/debian/changelog
+sed -i -e "s/^\(python-dfvfs \)([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-1)/\1(${DATE_VERSION}-1)/" config/dpkg/changelog
+sed -i -e "s/^\( -- ${EMAIL_DPKG}  \).*$/\1${DATE_DPKG}/" config/dpkg/changelog
