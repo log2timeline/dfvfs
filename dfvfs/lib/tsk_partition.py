60a19373225843a17f1ccf9f40136ea688df162d
Joachim Metz <joachim.metz@gmail.com>
2014-06-04 22:51:44 +0200

    Code review: 105840043:  Improved EWF filename globbing.

--- dfvfs/lib/tsk_partition.py
+++ dfvfs/lib/tsk_partition.py
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Helper function for the SleuthKit (TSK) partition support."""
+"""Helper functions for the SleuthKit (TSK) partition support."""
 
 import pytsk3
 
