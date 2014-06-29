eb03744fe6de2559c0e8d6858058b6e7dfa02f28
Joachim Metz <joachim.metz@gmail.com>
2014-06-29 19:54:29 +0200

    Code review: 106290045:  Added pycoverage support to run test script.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -19,17 +19,47 @@
 EXIT_FAILURE=1;
 EXIT_SUCCESS=0;
 
+COVERAGE="/usr/bin/coverage";
+COVERAGE_REPORT="tests-coverage.txt";
+PYTHON="/usr/bin/python";
+
+if ! test -x "${PYTHON}";
+then
+  # MSYS-MinGW allows to run the script using the Windows Python version.
+  PYTHON="/c/python27/python.exe";
+fi
+
+if ! test -x "${PYTHON}";
+then
+  echo "Unable to location Python interpreter."
+  echo "";
+  exit ${EXIT_FAILURE};
+fi
+
+if test -x "${COVERAGE}";
+then
+  rm -f .coverage ${COVERAGE_REPORT};
+fi
+
 # Run the tests in a specific order.
-for SUBDIR in lib path vfs volume file_io resolver analyzer helpers serializer;
+SUBDIRS="lib path vfs volume file_io resolver analyzer helpers serializer";
+
+for SUBDIR in ${SUBDIRS};
 do
   TEST_FILES=`find "dfvfs/${SUBDIR}" -name "*_test.py"`;
 
   for TEST_FILE in ${TEST_FILES};
   do
-    echo "---+ ${TEST_FILE} +---"
-    PYTHONPATH=. /usr/bin/python ${TEST_FILE}
+    echo "---+ ${TEST_FILE} +---";
 
-    if [ $? -ne 0 ]
+    if test -x "${COVERAGE}";
+    then
+      PYTHONPATH=. ${COVERAGE} run -a ${TEST_FILE};
+    else
+      PYTHONPATH=. ${PYTHON} ${TEST_FILE};
+    fi
+
+    if test $? -ne 0;
     then
       echo "TEST FAILED: ${TEST_FILE}.";
       echo "";
@@ -40,6 +70,14 @@ do
     echo "";
   done
 done
+
+if test -x "${COVERAGE}";
+then
+  echo "Writing tests coverage report: ${COVERAGE_REPORT}"; 
+  ${COVERAGE} report -m > ${COVERAGE_REPORT};
+
+  rm -f .coverage
+fi
 
 exit ${EXIT_SUCCESS};
 
