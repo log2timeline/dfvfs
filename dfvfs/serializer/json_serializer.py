ad3cbc6e5c54fc5953cf4dd851aee1fa8937e029
Joachim Metz <joachim.metz@gmail.com>
2015-04-08 18:30:12 +0200

    Code review: 220560043:  Changes to JSON serializer to remove serialized-in-serialized data.

--- dfvfs/serializer/json_serializer.py
+++ dfvfs/serializer/json_serializer.py
@@ -4,7 +4,116 @@
 import json
 
 from dfvfs.path import factory as path_spec_factory
+from dfvfs.path import path_spec
 from dfvfs.serializer import serializer
+
+class _PathSpecJsonDecoder(json.JSONDecoder):
+  """A class that implements a path specification JSON decoder."""
+
+  _CLASS_TYPES = frozenset([u'PathSpec'])
+
+  def __init__(self, *args, **kargs):
+    """Initializes the path specification JSON decoder object."""
+    super(_PathSpecJsonDecoder, self).__init__(
+        *args, object_hook=self._ConvertDictToObject, **kargs)
+
+  def _ConvertDictToObject(self, json_dict):
+    """Converts a JSON dict into a path specification object.
+
+    The dictionary of the JSON serialized objects consists of:
+    {
+        '__type__': 'PathSpec'
+        'type_indicator': 'OS'
+        'parent': { ... }
+        ...
+    }
+
+    Here '__type__' indicates the object base type in this case this should
+    be 'PathSpec'. The rest of the elements of the dictionary make up the
+    path specification object properties. Note that json_dict is a dict of
+    dicts and the _ConvertDictToObject method will be called for every dict.
+    That is how the path specification parent objects are created.
+
+    Args:
+      json_dict: a dictionary of the JSON serialized objects.
+
+    Returns:
+      A path specification (instance of path.PathSpec).
+
+    Raises:
+      TypeError: if the JSON serialized object does not contain a '__type__'
+                 attribute that contains 'PathSpec'.
+    """
+    # Use __type__ to indicate the object class type.
+    class_type = json_dict.get(u'__type__', None)
+
+    if class_type not in self._CLASS_TYPES:
+      raise TypeError(u'Missing path specification object type.')
+
+    # Remove the class type from the JSON dict since we cannot pass it.
+    del json_dict[u'__type__']
+
+    type_indicator = json_dict.get(u'type_indicator', None)
+    if type_indicator:
+      del json_dict[u'type_indicator']
+
+    return path_spec_factory.Factory.NewPathSpec(type_indicator, **json_dict)
+
+
+class _PathSpecJsonEncoder(json.JSONEncoder):
+  """A class that implements a path specification object JSON encoder."""
+
+  # Note: that the following functions do not follow the style guide
+  # because they are part of the json.JSONEncoder object interface.
+
+  # pylint: disable=method-hidden
+  def default(self, path_spec_object):
+    """Converts a path specification object into a JSON dictionary.
+
+    The resulting dictionary of the JSON serialized objects consists of:
+    {
+        '__type__': 'PathSpec'
+        'type_indicator': 'OS'
+        'parent': { ... }
+        ...
+    }
+
+    Here '__type__' indicates the object base type in this case this should
+    be 'PathSpec'. The rest of the elements of the dictionary make up the
+    path specification object properties. The supported property names are
+    defined in path_spec_factory.Factory.PROPERTY_NAMES. Note that this method
+    is called recursively for every path specification object and creates
+    a dict of dicts in the process that is transformed into a JSON string
+    by the JSON encoder.
+
+    Args:
+      path_spec_object: a path specification (instance of path.PathSpec).
+
+    Returns:
+      A dictionary of the JSON serialized objects.
+
+    Raises:
+      TypeError: if not an instance of path.PathSpec.
+    """
+    if not isinstance(path_spec_object, path_spec.PathSpec):
+      raise TypeError
+
+    json_dict = {u'__type__': u'PathSpec'}
+    for property_name in path_spec_factory.Factory.PROPERTY_NAMES:
+      if hasattr(path_spec_object, property_name):
+        property_value = getattr(path_spec_object, property_name, None)
+        if property_value is not None:
+          json_dict[property_name] = property_value
+
+    if path_spec_object.HasParent():
+      json_dict[u'parent'] = self.default(path_spec_object.parent)
+
+    json_dict[u'type_indicator'] = path_spec_object.type_indicator
+    location = getattr(path_spec_object, u'location', None)
+    if location:
+      json_dict[u'location'] = location
+
+    return json_dict
 
 
 class JsonPathSpecSerializer(serializer.PathSpecSerializer):
@@ -15,50 +124,22 @@ class JsonPathSpecSerializer(serializer.PathSpecSerializer):
     """Reads a path specification from serialized form.
 
     Args:
-      json_string: a JSON string containing the serialized form.
+      json_string: a JSON string containing the serialized path specification.
 
     Returns:
       A path specification (instance of path.PathSpec).
     """
-    json_dict = json.loads(json_string)
-    type_indicator = u''
-
-    if 'type_indicator' in json_dict:
-      type_indicator = json_dict.get('type_indicator')
-      del json_dict['type_indicator']
-
-    if 'parent' in json_dict:
-      json_dict['parent'] = cls.ReadSerialized(json_dict.get('parent'))
-
-    return path_spec_factory.Factory.NewPathSpec(type_indicator, **json_dict)
+    json_decoder = _PathSpecJsonDecoder()
+    return json_decoder.decode(json_string)
 
   @classmethod
-  def WriteSerialized(cls, path_spec):
+  def WriteSerialized(cls, path_spec_object):
     """Writes a path specification to serialized form.
 
     Args:
-      path_spec: a path specification (instance of path.PathSpec).
+      path_spec_object: a path specification (instance of path.PathSpec).
 
     Returns:
-      A JSON string containing the serialized form.
+      A JSON string containing the serialized path specification.
     """
-    type_indicator = path_spec.type_indicator
-
-    attributes = {}
-
-    # We don't want to set attributes here that are not used.
-    for property_name in path_spec_factory.Factory.PROPERTY_NAMES:
-      if hasattr(path_spec, property_name):
-        property_value = getattr(path_spec, property_name, None)
-        if property_value is not None:
-          attributes[property_name] = property_value
-
-    if path_spec.HasParent():
-      attributes['parent'] = cls.WriteSerialized(path_spec.parent)
-
-    attributes['type_indicator'] = type_indicator
-    location = getattr(path_spec, 'location', None)
-    if location:
-      attributes['location'] = location
-
-    return json.dumps(attributes)
+    return json.dumps(path_spec_object, cls=_PathSpecJsonEncoder)
