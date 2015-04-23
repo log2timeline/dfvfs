11ef66fae9ad0097c458b151cf05fe2f1b950da1
Joachim Metz <joachim.metz@gmail.com>
2015-04-23 21:30:59 +0200

    Code review: 222170044: Moved dependencies to dfvfs/dependencies #19.

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,16 @@
 import unittest
 import sys
 
+# Change PYTHONPATH to include dfVFS.
+sys.path.insert(0, u'.')
+
+import dfvfs.dependencies
+
 
 if __name__ == '__main__':
+  if not dfvfs.dependencies.CheckDependencies():
+    sys.exit(1)
+
   test_suite = unittest.TestLoader().discover('tests', pattern='*.py')
   test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
   if not test_results.wasSuccessful():
