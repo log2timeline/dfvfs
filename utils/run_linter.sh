ca22b835ebf871e57cad093587764958b669f1c3
Joachim Metz <joachim.metz@gmail.com>
2015-05-14 13:23:59 +0200

    Changes to run linter script.

--- utils/run_linter.sh
+++ utils/run_linter.sh
@@ -13,6 +13,31 @@ fi
 
 . utils/common.sh
 
+# Determine if we have the master repo as origin.
+HAVE_REMOTE_ORIGIN=have_remote_origin;
+
+if ${HAVE_REMOTE_ORIGIN};
+then
+  HAVE_UNCOMMITTED_CHANGES=1;
+else
+  if ! have_remote_upstream;
+  then
+    echo "Review upload aborted - missing upstream.";
+    echo "Run: 'git remote add upstream https://github.com/log2timeline/dfvfs.git'";
+
+    exit ${EXIT_FAILURE};
+  fi
+
+  HAVE_UNCOMMITTED_CHANGES=have_uncommitted_changes;
+fi
+
+if ${HAVE_UNCOMMITTED_CHANGES};
+then
+  LINTING_IS_CORRECT=linting_is_correct;
+else
+  LINTING_IS_CORRECT=linter_pass;
+fi
+
 if ! linting_is_correct;
 then
   echo "Aborted - fix the issues reported by the linter.";
