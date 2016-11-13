44e68e54f4648a4bbeedffcfdf9abcf01b95f428
dc3.plaso <dc3.plaso@gmail.com>
2016-11-13 08:31:17 +0100

    Code review: 311240043: Add DataRange file system and file entry #176

---
+++ dfvfs/vfs/data_range_file_entry.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""The data range file entry implementation."""
+
+from dfvfs.lib import definitions
+from dfvfs.vfs import root_only_file_entry
+from dfvfs.vfs import vfs_stat
+
+
+class DataRangeFileEntry(root_only_file_entry.RootOnlyFileEntry):
+  """Class that implements a data range file entry object."""
+
+  TYPE_INDICATOR = definitions.TYPE_INDICATOR_DATA_RANGE
+
+  def _GetStat(self):
+    """Retrieves the stat object.
+
+    Returns:
+      The stat object (instance of vfs.VFSStat).
+
+    Raises:
+      BackEndError: when the encoded stream is missing.
+    """
+    stat_object = vfs_stat.VFSStat()
+
+    # File data stat information.
+    stat_object.size = self.path_spec.range_size
+
+    # File entry type stat information.
+    stat_object.type = stat_object.TYPE_FILE
+
+    return stat_object
---
+++ tests/vfs/data_range_file_system.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Tests for the data range file system implementation."""
+
+import unittest
+
+from dfvfs.path import data_range_path_spec
+from dfvfs.path import os_path_spec
+from dfvfs.resolver import context
+from dfvfs.vfs import data_range_file_system
+
+from tests import test_lib as shared_test_lib
+
+
+@shared_test_lib.skipUnlessHasTestFile([u'syslog'])
+class DataRangeFileSystemTest(shared_test_lib.BaseTestCase):
+  """The unit test for the data range file system object."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    self._resolver_context = context.Context()
+    test_file = self._GetTestFilePath([u'syslog'])
+    path_spec = os_path_spec.OSPathSpec(location=test_file)
+    self._data_range_path_spec = (
+        data_range_path_spec.DataRangePathSpec(
+            range_offset=0x1c0, range_size=0x41, parent=path_spec))
+
+  def testOpenAndClose(self):
+    """Test the open and close functionality."""
+    file_system = data_range_file_system.DataRangeFileSystem(
+        self._resolver_context)
+    self.assertIsNotNone(file_system)
+
+    file_system.Open(self._data_range_path_spec)
+
+    file_system.Close()
+
+  def testFileEntryExistsByPathSpec(self):
+    """Test the file entry exists by path specification functionality."""
+    file_system = data_range_file_system.DataRangeFileSystem(
+        self._resolver_context)
+    self.assertIsNotNone(file_system)
+
+    file_system.Open(self._data_range_path_spec)
+
+    self.assertTrue(file_system.FileEntryExistsByPathSpec(
+        self._data_range_path_spec))
+
+    file_system.Close()
+
+  def testGetFileEntryByPathSpec(self):
+    """Tests the GetFileEntryByPathSpec function."""
+    file_system = data_range_file_system.DataRangeFileSystem(
+        self._resolver_context)
+    self.assertIsNotNone(file_system)
+
+    file_system.Open(self._data_range_path_spec)
+
+    file_entry = file_system.GetFileEntryByPathSpec(
+        self._data_range_path_spec)
+
+    self.assertIsNotNone(file_entry)
+    self.assertEqual(file_entry.name, u'')
+
+    file_system.Close()
+
+  def testGetRootFileEntry(self):
+    """Test the get root file entry functionality."""
+    file_system = data_range_file_system.DataRangeFileSystem(
+        self._resolver_context)
+    self.assertIsNotNone(file_system)
+
+    file_system.Open(self._data_range_path_spec)
+
+    file_entry = file_system.GetRootFileEntry()
+
+    self.assertIsNotNone(file_entry)
+    self.assertEqual(file_entry.name, u'')
+
+    file_system.Close()
+
+
+if __name__ == '__main__':
+  unittest.main()
