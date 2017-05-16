4411775123c6e956fb4dc5cbf4775f1d4e4591d5
Joachim Metz <joachim.metz@gmail.com>
2017-05-16 07:30:00 +0200

    Changed dependency management to use configuration file

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -10,11 +10,9 @@ L2TBINARIES_TEST_DEPENDENCIES="funcsigs mock pbr";
 
 PYTHON2_DEPENDENCIES="libbde-python libewf-python libfsntfs-python libfvde-python libfwnt-python libqcow-python libsigscan-python libsmdev-python libsmraw-python libvhdi-python libvmdk-python libvshadow-python libvslvm-python python-backports.lzma python-construct python-crypto python-dfdatetime python-pysqlite python-pytsk3 python-six";
 
-PYTHON2_TEST_DEPENDENCIES="python-mock";
-
 PYTHON3_DEPENDENCIES="libbde-python3 libewf-python3 libfsntfs-python3 libfvde-python3 libfwnt-python3 libqcow-python3 libsigscan-python3 libsmdev-python3 libsmraw-python3 libvhdi-python3 libvmdk-python3 libvshadow-python3 libvslvm-python3 python3-construct python3-crypto python3-dfdatetime python3-pytsk3 python3-six";
 
-PYTHON3_TEST_DEPENDENCIES="python3-mock";
+PYTHON_TEST_DEPENDENCIES="python-mock";
 
 # Exit on error.
 set -e;
@@ -32,11 +30,11 @@ elif test ${TRAVIS_OS_NAME} = "linux" && test ${TRAVIS_PYTHON_VERSION} = "2.7";
 then
 	sudo add-apt-repository ppa:gift/dev -y;
 	sudo apt-get update -q;
-	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES};
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};
 
 elif test ${TRAVIS_OS_NAME} = "linux" && test ${TRAVIS_PYTHON_VERSION} = "3.4";
 then
 	sudo add-apt-repository ppa:gift/dev -y;
 	sudo apt-get update -q;
-	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON3_DEPENDENCIES} ${PYTHON3_TEST_DEPENDENCIES};
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON3_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};
 fi
--- utils/update_dependencies.py
+++ utils/update_dependencies.py
@@ -315,7 +315,7 @@ class TravisBeforeInstallScriptWriter(DependencyFileWriter):
       u'\tsudo add-apt-repository ppa:gift/dev -y;',
       u'\tsudo apt-get update -q;',
       (u'\tsudo apt-get install -y ${COVERALL_DEPENDENCIES} '
-       u'${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES};'),
+       u'${PYTHON2_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};'),
       u'',
       (u'elif test ${TRAVIS_OS_NAME} = "linux" && '
        u'test ${TRAVIS_PYTHON_VERSION} = "3.4";'),
@@ -323,7 +323,7 @@ class TravisBeforeInstallScriptWriter(DependencyFileWriter):
       u'\tsudo add-apt-repository ppa:gift/dev -y;',
       u'\tsudo apt-get update -q;',
       (u'\tsudo apt-get install -y ${COVERALL_DEPENDENCIES} '
-       u'${PYTHON3_DEPENDENCIES} ${PYTHON3_TEST_DEPENDENCIES};'),
+       u'${PYTHON3_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};'),
       u'fi',
       u'']
 
@@ -348,9 +348,6 @@ class TravisBeforeInstallScriptWriter(DependencyFileWriter):
     file_content.append(u'PYTHON2_DEPENDENCIES="{0:s}";'.format(dependencies))
 
     file_content.append(u'')
-    file_content.append(u'PYTHON2_TEST_DEPENDENCIES="python-mock";')
-
-    file_content.append(u'')
 
     dependencies = self._dependency_helper.GetDPKGDepends(exclude_version=True)
     if u'python-backports.lzma' in dependencies:
@@ -363,7 +360,7 @@ class TravisBeforeInstallScriptWriter(DependencyFileWriter):
     file_content.append(u'PYTHON3_DEPENDENCIES="{0:s}";'.format(dependencies))
 
     file_content.append(u'')
-    file_content.append(u'PYTHON3_TEST_DEPENDENCIES="python3-mock";')
+    file_content.append(u'PYTHON_TEST_DEPENDENCIES="python-mock";')
 
     file_content.extend(self._FILE_FOOTER)
 
