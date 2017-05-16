59c261bb728e308a9a82fb425e3145280fbcbaa4
Joachim Metz <joachim.metz@gmail.com>
2017-05-16 08:00:27 +0200

    Changed dependency management to use configuration file

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -10,9 +10,7 @@ L2TBINARIES_TEST_DEPENDENCIES="funcsigs mock pbr";
 
 PYTHON2_DEPENDENCIES="libbde-python libewf-python libfsntfs-python libfvde-python libfwnt-python libqcow-python libsigscan-python libsmdev-python libsmraw-python libvhdi-python libvmdk-python libvshadow-python libvslvm-python python-backports.lzma python-construct python-crypto python-dfdatetime python-pysqlite python-pytsk3 python-six";
 
-PYTHON3_DEPENDENCIES="libbde-python3 libewf-python3 libfsntfs-python3 libfvde-python3 libfwnt-python3 libqcow-python3 libsigscan-python3 libsmdev-python3 libsmraw-python3 libvhdi-python3 libvmdk-python3 libvshadow-python3 libvslvm-python3 python3-construct python3-crypto python3-dfdatetime python3-pytsk3 python3-six";
-
-PYTHON_TEST_DEPENDENCIES="python-mock";
+PYTHON2_TEST_DEPENDENCIES="python-mock";
 
 # Exit on error.
 set -e;
@@ -26,15 +24,11 @@ then
 
 	PYTHONPATH=../l2tdevtools ../l2tdevtools/tools/update.py --download-directory=dependencies ${L2TBINARIES_DEPENDENCIES} ${L2TBINARIES_TEST_DEPENDENCIES};
 
-elif test ${TRAVIS_OS_NAME} = "linux" && test ${TRAVIS_PYTHON_VERSION} = "2.7";
+elif test ${TRAVIS_OS_NAME} = "linux";
 then
 	sudo add-apt-repository ppa:gift/dev -y;
 	sudo apt-get update -q;
-	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};
-
-elif test ${TRAVIS_OS_NAME} = "linux" && test ${TRAVIS_PYTHON_VERSION} = "3.4";
-then
-	sudo add-apt-repository ppa:gift/dev -y;
-	sudo apt-get update -q;
-	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON3_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};
+	# Only install the Python 2 dependencies.
+	# Also see: https://docs.travis-ci.com/user/languages/python/#Travis-CI-Uses-Isolated-virtualenvs
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES};
 fi
--- utils/update_dependencies.py
+++ utils/update_dependencies.py
@@ -309,21 +309,15 @@ class TravisBeforeInstallScriptWriter(DependencyFileWriter):
        u'--download-directory=dependencies ${L2TBINARIES_DEPENDENCIES} '
        u'${L2TBINARIES_TEST_DEPENDENCIES};'),
       u'',
-      (u'elif test ${TRAVIS_OS_NAME} = "linux" && '
-       u'test ${TRAVIS_PYTHON_VERSION} = "2.7";'),
+      u'elif test ${TRAVIS_OS_NAME} = "linux";',
       u'then',
       u'\tsudo add-apt-repository ppa:gift/dev -y;',
       u'\tsudo apt-get update -q;',
+      u'\t# Only install the Python 2 dependencies.',
+      (u'\t# Also see: https://docs.travis-ci.com/user/languages/python/'
+       u'#Travis-CI-Uses-Isolated-virtualenvs'),
       (u'\tsudo apt-get install -y ${COVERALL_DEPENDENCIES} '
-       u'${PYTHON2_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};'),
-      u'',
-      (u'elif test ${TRAVIS_OS_NAME} = "linux" && '
-       u'test ${TRAVIS_PYTHON_VERSION} = "3.4";'),
-      u'then',
-      u'\tsudo add-apt-repository ppa:gift/dev -y;',
-      u'\tsudo apt-get update -q;',
-      (u'\tsudo apt-get install -y ${COVERALL_DEPENDENCIES} '
-       u'${PYTHON3_DEPENDENCIES} ${PYTHON_TEST_DEPENDENCIES};'),
+       u'${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES};'),
       u'fi',
       u'']
 
@@ -348,19 +342,7 @@ class TravisBeforeInstallScriptWriter(DependencyFileWriter):
     file_content.append(u'PYTHON2_DEPENDENCIES="{0:s}";'.format(dependencies))
 
     file_content.append(u'')
-
-    dependencies = self._dependency_helper.GetDPKGDepends(exclude_version=True)
-    if u'python-backports.lzma' in dependencies:
-      dependencies.remove(u'python-backports.lzma')
-    if u'python-pysqlite' in dependencies:
-      dependencies.remove(u'python-pysqlite')
-
-    dependencies = u' '.join(dependencies)
-    dependencies = dependencies.replace(u'python', u'python3')
-    file_content.append(u'PYTHON3_DEPENDENCIES="{0:s}";'.format(dependencies))
-
-    file_content.append(u'')
-    file_content.append(u'PYTHON_TEST_DEPENDENCIES="python-mock";')
+    file_content.append(u'PYTHON2_TEST_DEPENDENCIES="python-mock";')
 
     file_content.extend(self._FILE_FOOTER)
 
