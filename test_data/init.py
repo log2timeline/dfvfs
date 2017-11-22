9e33bc901d9fa9c61243be7b3cf0c2f617ead04e
Joachim Metz <joachim.metz@gmail.com>
2017-11-22 20:55:22 +0100

    Changed fake_file_system_builder testAddFileReadData to use file in test_data #227

---
+++ test_data/init.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Digital Forensics Virtual File System (dfVFS).
+
+dfVFS, or Digital Forensics Virtual File System, is a Python module
+that provides read-only access to file-system objects from various
+storage media types and file formats.
+"""
