36293aeee19409d0478078a58ed63f7bfba88fb8
Joachim Metz <joachim.metz@gmail.com>
2018-02-18 12:35:43 +0100

    Made lzma a required dependency

--- dfvfs/compression/__init__.py
+++ dfvfs/compression/__init__.py
@@ -2,10 +2,5 @@
 """Imports for the compression manager."""
 
 from dfvfs.compression import bzip2_decompressor
-
-try:
-  from dfvfs.compression import xz_decompressor
-except ImportError:
-  pass
-
+from dfvfs.compression import xz_decompressor
 from dfvfs.compression import zlib_decompressor
