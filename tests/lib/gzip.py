bf5566c47db123c5419ebcdce3ac9b70a00b71cd
Joachim Metz <joachim.metz@gmail.com>
2018-03-25 11:49:35 +0200

    Refactored gzip format support into lib

---
+++ tests/lib/gzip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for gzip compressed stream file."""
+
+from __future__ import unicode_literals
+
+import unittest
+
+
+# TODO: add tests for _GzipDecompressorState
+# TODO: add tests for GzipMember
+
+
+if __name__ == '__main__':
+  unittest.main()
