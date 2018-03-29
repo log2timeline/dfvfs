5d98cc4fa65e001adca008be4c2db1b799673beb
Joachim Metz <joachim.metz@gmail.com>
2018-03-29 07:49:03 +0200

    Changes after review

--- dfvfs/analyzer/analyzer_helper.py
+++ dfvfs/analyzer/analyzer_helper.py
@@ -9,7 +9,9 @@ class AnalyzerHelper(object):
 
   @property
   def format_categories(self):
-    """set[str]: format categories."""
+    """set[str]: format categories, such as archive file or file system and
+        are defined in defintions.FORMAT_CATEGORIES.
+    """
     format_categories = getattr(self, 'FORMAT_CATEGORIES', None)
     if format_categories is None:
       raise NotImplementedError(
