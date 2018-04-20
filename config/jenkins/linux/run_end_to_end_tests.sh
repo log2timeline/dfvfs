f75acb6309c2383e0304d8074d24d043688fa504
Joachim Metz <joachim.metz@gmail.com>
2018-04-20 08:11:16 +0200

    Added Ubuntu end-to-end tests run script for Jenkins

--- config/jenkins/linux/run_end_to_end_tests.sh
+++ config/jenkins/linux/run_end_to_end_tests.sh
@@ -36,7 +36,7 @@ PYTHONPATH=. ./utils/check_dependencies.py
 
 # Start the end-to-end tests in the background so we can capture the PID of
 # the process while the script is running.
-PYTHONPATH=. ./tests/end-to-end.py --config ${CONFIGURATION_FILE} --sources-directory ${SOURCES_DIRECTORY} --tools-directory ./tools --results-directory ${RESULTS_DIRECTORY} --references-directory ${REFERENCES_DIRECTORY} &
+PYTHONPATH=. ./tests/end-to-end.py --config ${CONFIGURATION_FILE} --sources-directory ${SOURCES_DIRECTORY} --scripts-directory ./examples --results-directory ${RESULTS_DIRECTORY} --references-directory ${REFERENCES_DIRECTORY} &
 
 echo "End-to-end tests stared (PID: $!)";
 
