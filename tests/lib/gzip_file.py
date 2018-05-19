4b79b2f78475e4d1855ad904a447ba2e7cef41ec
Joachim Metz <joachim.metz@gmail.com>
2018-05-19 16:26:02 +0200

    Renamed gzip to gzip_file to prevent module name conflict

--- tests/lib/gzip_file.py
+++ tests/lib/gzip_file.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 """Tests for gzip compressed stream file."""
 
+# Note: do not rename file to gzip.py this can cause the exception:
+# AttributeError: 'module' object has no attribute 'GzipFile'
+# when using pip.
+
 from __future__ import unicode_literals
 
 import unittest
