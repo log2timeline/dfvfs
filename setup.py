0f0547a8a413eaa7903fa899862d76ea55700017
Joachim Metz <joachim.metz@gmail.com>
2019-01-21 22:56:50 +0100

    Changes to run CI tests with pylint 2.x and removed Trusty CI tests (#371)

--- setup.py
+++ setup.py
@@ -84,11 +84,12 @@ else:
         spec_file = bdist_rpm._make_spec_file(self)
 
       if sys.version_info[0] < 3:
-        python_package = 'python'
+        python_package = 'python2'
       else:
         python_package = 'python3'
 
       description = []
+      requires = ''
       summary = ''
       in_description = False
 
@@ -98,35 +99,60 @@ else:
           summary = line
 
         elif line.startswith('BuildRequires: '):
-          line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+          line = 'BuildRequires: {0:s}-setuptools, {0:s}-devel'.format(
+              python_package)
 
         elif line.startswith('Requires: '):
+          requires = line[10:]
           if python_package == 'python3':
-            line = line.replace('python', 'python3')
+            requires = requires.replace('python-', 'python3-')
+            requires = requires.replace('python2-', 'python3-')
 
         elif line.startswith('%description'):
           in_description = True
 
+        elif line.startswith('python setup.py build'):
+          if python_package == 'python3':
+            line = '%py3_build'
+          else:
+            line = '%py2_build'
+
+        elif line.startswith('python setup.py install'):
+          if python_package == 'python3':
+            line = '%py3_install'
+          else:
+            line = '%py2_install'
+
         elif line.startswith('%files'):
-          # Cannot use %{_libdir} here since it can expand to "lib64".
           lines = [
               '%files -n {0:s}-%{{name}}'.format(python_package),
               '%defattr(644,root,root,755)',
-              '%doc ACKNOWLEDGEMENTS AUTHORS LICENSE README',
-              '%{_prefix}/lib/python*/site-packages/dfvfs/*.py',
-              '%{_prefix}/lib/python*/site-packages/dfvfs/*/*.py',
-              '%{_prefix}/lib/python*/site-packages/dfvfs/*/*.yaml',
-              '%{_prefix}/lib/python*/site-packages/dfvfs*.egg-info/*',
-              '',
-              '%exclude %{_prefix}/share/doc/*',
-              '%exclude %{_prefix}/lib/python*/site-packages/dfvfs/*.pyc',
-              '%exclude %{_prefix}/lib/python*/site-packages/dfvfs/*.pyo',
-              ('%exclude %{_prefix}/lib/python*/site-packages/dfvfs/'
-               '__pycache__/*'),
-              '%exclude %{_prefix}/lib/python*/site-packages/dfvfs/*/*.pyc',
-              '%exclude %{_prefix}/lib/python*/site-packages/dfvfs/*/*.pyo',
-              ('%exclude %{_prefix}/lib/python*/site-packages/dfvfs/*/'
-               '__pycache__/*')]
+              '%license LICENSE',
+              '%doc ACKNOWLEDGEMENTS AUTHORS README']
+
+          if python_package == 'python3':
+            lines.extend([
+                '%{python3_sitelib}/dfvfs/*.py',
+                '%{python3_sitelib}/dfvfs/*/*.py',
+                '%{python3_sitelib}/dfvfs/*/*.yaml',
+                '%{python3_sitelib}/dfvfs*.egg-info/*',
+                '',
+                '%exclude %{_prefix}/share/doc/*',
+                '%exclude %{python3_sitelib}/dfvfs/__pycache__/*',
+                '%exclude %{python3_sitelib}/dfvfs/*/__pycache__/*'])
+
+          else:
+            lines.extend([
+                '%{python2_sitelib}/dfvfs/*.py',
+                '%{python2_sitelib}/dfvfs/*/*.py',
+                '%{python2_sitelib}/dfvfs/*/*.yaml',
+                '%{python2_sitelib}/dfvfs*.egg-info/*',
+                '',
+                '%exclude %{_prefix}/share/doc/*',
+                '%exclude %{python2_sitelib}/dfvfs/*.pyc',
+                '%exclude %{python2_sitelib}/dfvfs/*.pyo',
+                '%exclude %{python2_sitelib}/dfvfs/*/*.pyc',
+                '%exclude %{python2_sitelib}/dfvfs/*/*.pyo'])
 
           python_spec_file.extend(lines)
           break
@@ -136,10 +162,17 @@ else:
 
           python_spec_file.append(
               '%package -n {0:s}-%{{name}}'.format(python_package))
-          python_spec_file.append('{0:s}'.format(summary))
-          python_spec_file.append('')
-          python_spec_file.append(
-              '%description -n {0:s}-%{{name}}'.format(python_package))
+          if python_package == 'python2':
+            python_spec_file.extend([
+                'Obsoletes: python-dfvfs < %{version}',
+                'Provides: python-dfvfs = %{version}'])
+
+          python_spec_file.extend([
+              'Requires: {0:s}'.format(requires),
+              '{0:s}'.format(summary),
+              '',
+              '%description -n {0:s}-%{{name}}'.format(python_package)])
+
           python_spec_file.extend(description)
 
         elif in_description:
