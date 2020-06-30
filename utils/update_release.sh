1d0b9e323180fd9fc741939aa4f939b56c868b65
Joachim Metz <joachim.metz@gmail.com>
2020-06-30 09:01:33 +0200

    Added update release script (#458)

---
+++ utils/update_release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Script that makes changes in preparation of a new release, such as updating
+# the version and documentation.
+
+EXIT_FAILURE=1;
+EXIT_SUCCESS=0;
+
+VERSION=`date -u +"%Y%m%d"`
+DPKG_DATE=`date -R`
+
+# Update the Python module version.
+sed "s/__version__ = '[0-9]*'/__version__ = '${VERSION}'/" -i dfvfs/__init__.py
+
+# Update the version in the dpkg configuration files.
+cat > config/dpkg/changelog << EOT
+dfvfs (${VERSION}-1) unstable; urgency=low
+
+  * Auto-generated
+
+ -- Log2Timeline maintainers <log2timeline-maintainers@googlegroups.com>  ${DPKG_DATE}
+EOT
+
+# Regenerate the supported formats documentation.
+# TODO: generate supported formats.
+# PYTHONPATH=. ./utils/export_supported_formats.py > docs/sources/Supported-formats.md
+
+# Regenerate the API documentation.
+tox -edocs
+
+exit ${EXIT_SUCCESS};
+
