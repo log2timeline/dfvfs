552dc17c11a250d04e1e4268129dfb1d78a0b3ae
Joachim Metz <joachim.metz@gmail.com>
2021-01-22 15:38:05 +0100

    Added OS bypass for RAW and EWF back-ends #120

--- dfvfs/file_io/raw_file_io.py
+++ dfvfs/file_io/raw_file_io.py
@@ -45,29 +45,34 @@ class RawFile(file_object_io.FileObjectIO):
           'Unsupported path specification without parent.')
 
     parent_path_spec = path_spec.parent
+    parent_location = getattr(parent_path_spec, 'location', None)
 
-    file_system = resolver.Resolver.OpenFileSystem(
-        parent_path_spec, resolver_context=self._resolver_context)
+    if parent_location and parent_path_spec.IsSystemLevel():
+      segment_file_paths = pysmraw.glob(parent_location)
 
-    # Note that we cannot use pysmraw's glob function since it does not
-    # handle the file system abstraction dfvfs provides.
-    segment_file_path_specs = raw.RawGlobPathSpec(file_system, path_spec)
-    if not segment_file_path_specs:
-      return None
+      raw_handle = pysmraw.handle()
+      raw_handle.open(segment_file_paths)
 
-    if parent_path_spec.IsSystemLevel():
-      # Typically the file-like object cache should have room for 127 items.
-      self._resolver_context.SetMaximumNumberOfFileObjects(
-          len(segment_file_path_specs) + 127)
+    else:
+      # Note that we cannot use pysmraw's glob function here since it does
+      # not handle the file system abstraction dfvfs provides.
 
-    file_objects = []
-    for segment_file_path_spec in segment_file_path_specs:
-      file_object = resolver.Resolver.OpenFileObject(
-          segment_file_path_spec, resolver_context=self._resolver_context)
-      file_objects.append(file_object)
+      file_system = resolver.Resolver.OpenFileSystem(
+          parent_path_spec, resolver_context=self._resolver_context)
 
-    raw_handle = pysmraw.handle()
-    raw_handle.open_file_objects(file_objects)
+      segment_file_path_specs = raw.RawGlobPathSpec(file_system, path_spec)
+      if not segment_file_path_specs:
+        return None
+
+      file_objects = []
+      for segment_file_path_spec in segment_file_path_specs:
+        file_object = resolver.Resolver.OpenFileObject(
+            segment_file_path_spec, resolver_context=self._resolver_context)
+        file_objects.append(file_object)
+
+      raw_handle = pysmraw.handle()
+      raw_handle.open_file_objects(file_objects)
+
     return raw_handle
 
   def get_size(self):
