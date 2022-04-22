93df61d2b97f17d5068ac49b9e36204510a62465
Syd Pleno <syd.pleno@gmail.com>
2022-04-22 14:21:14 +1000

    Add overlay analyzer helper

---
+++ dfvfs/analyzer/overlay_analyzer_helper.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""The OVerlay format analyzer helper implementation."""
+
+from dfvfs.analyzer import analyzer
+from dfvfs.analyzer import analyzer_helper
+from dfvfs.analyzer import specification
+from dfvfs.lib import definitions
+
+
+class OverlayAnalyzerHelper(analyzer_helper.AnalyzerHelper):
+  """Overlay analyzer helper."""
+
+  FORMAT_CATEGORIES = frozenset([
+      definitions.FORMAT_CATEGORY_FILE_SYSTEM])
+
+  TYPE_INDICATOR = definitions.TYPE_INDICATOR_OVERLAY
+
+
+analyzer.Analyzer.RegisterHelper(OverlayAnalyzerHelper())
