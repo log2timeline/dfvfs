6fae98eba37a1e1246225f3bac384fb4a6fce71a
Joachim Metz <joachim.metz@gmail.com>
2022-06-05 10:43:59 +0200

    Updated pylintrc configuration to version 2.10.x

--- tests/resolver/context.py
+++ tests/resolver/context.py
@@ -26,13 +26,13 @@ class ContextTest(shared_test_lib.BaseTestCase):
     """Tests the cache file-like object functionality."""
     resolver_context = context.Context()
 
-    self.assertEqual(len(resolver_context._file_object_cache._values), 0)
+    self.assertEqual(len(resolver_context._file_object_cache), 0)
 
     path_spec = fake_path_spec.FakePathSpec(location='/empty.txt')
     file_object = fake_file_io.FakeFile(resolver_context, path_spec, b'')
 
     resolver_context.CacheFileObject(path_spec, file_object)
-    self.assertEqual(len(resolver_context._file_object_cache._values), 1)
+    self.assertEqual(len(resolver_context._file_object_cache), 1)
 
     cached_object = resolver_context.GetFileObject(path_spec)
     self.assertEqual(cached_object, file_object)
@@ -41,13 +41,13 @@ class ContextTest(shared_test_lib.BaseTestCase):
     """Tests the cache file system object functionality."""
     resolver_context = context.Context()
 
-    self.assertEqual(len(resolver_context._file_system_cache._values), 0)
+    self.assertEqual(len(resolver_context._file_system_cache), 0)
 
     path_spec = fake_path_spec.FakePathSpec(location='/')
     file_system = fake_file_system.FakeFileSystem(resolver_context, path_spec)
 
     resolver_context.CacheFileSystem(path_spec, file_system)
-    self.assertEqual(len(resolver_context._file_system_cache._values), 1)
+    self.assertEqual(len(resolver_context._file_system_cache), 1)
 
     cached_object = resolver_context.GetFileSystem(path_spec)
     self.assertEqual(cached_object, file_system)
