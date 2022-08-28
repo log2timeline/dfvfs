b185568a1b77ac0c4f2f234cbc5bb7b4f902a42f
Joachim Metz <joachim.metz@gmail.com>
2022-08-28 20:39:05 +0200

    Changes to scripts for CoreStorage test data #409 (#677)

--- tests/file_io/cs_file_io.py
+++ tests/file_io/cs_file_io.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+from dfvfs.file_io import hfs_file_io
 from dfvfs.lib import definitions
 from dfvfs.lib import errors
 from dfvfs.path import factory as path_spec_factory
@@ -12,7 +13,74 @@ from dfvfs.resolver import resolver
 from tests.file_io import test_lib
 
 
-class CSFileTestWithKeyChainTest(test_lib.ImageFileTestCase):
+class CSFileTest(test_lib.HFSImageFileTestCase):
+  """Tests the Core Storage (CS) file-like object."""
+
+  _IDENTIFIER_ANOTHER_FILE = 26
+  _IDENTIFIER_PASSWORDS_TXT = 25
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    super(CSFileTest, self).setUp()
+    test_path = self._GetTestFilePath(['cs_single_volume.raw'])
+    self._SkipIfPathNotExists(test_path)
+
+    test_os_path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_OS, location=test_path)
+    self._raw_path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_RAW, parent=test_os_path_spec)
+    self._cs_path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_CS, parent=self._raw_path_spec,
+        volume_index=0)
+
+  def testOpenCloseIdentifier(self):
+    """Test the open and close functionality using an identifier."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestOpenCloseIdentifier(file_object)
+
+  def testOpenCloseLocation(self):
+    """Test the open and close functionality using a location."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, location='/passwords.txt',
+        parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestOpenCloseLocation(file_object)
+
+    # Try open with a path specification that has no parent.
+    path_spec.parent = None
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    with self.assertRaises(errors.PathSpecError):
+      self._TestOpenCloseLocation(file_object)
+
+  def testSeek(self):
+    """Test the seek functionality."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_ANOTHER_FILE,
+        location='/a_directory/another_file', parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestSeek(file_object)
+
+  def testRead(self):
+    """Test the read functionality."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, location='/passwords.txt',
+        parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestRead(file_object)
+
+
+class CSFileWithKeyChainTest(test_lib.HFSImageFileTestCase):
   """Tests the Core Storage (CS) file-like object.
 
   The credentials are passed via the key chain.
@@ -20,12 +88,12 @@ class CSFileTestWithKeyChainTest(test_lib.ImageFileTestCase):
 
   _FVDE_PASSWORD = 'fvde-TEST'
 
-  _INODE_PASSWORDS_TXT = 26
-  _INODE_ANOTHER_FILE = 24
+  _IDENTIFIER_ANOTHER_FILE = 24
+  _IDENTIFIER_PASSWORDS_TXT = 26
 
   def setUp(self):
     """Sets up the needed objects used throughout the test."""
-    super(CSFileTestWithKeyChainTest, self).setUp()
+    super(CSFileWithKeyChainTest, self).setUp()
     test_path = self._GetTestFilePath(['fvdetest.qcow2'])
     self._SkipIfPathNotExists(test_path)
 
@@ -33,51 +101,72 @@ class CSFileTestWithKeyChainTest(test_lib.ImageFileTestCase):
         definitions.TYPE_INDICATOR_OS, location=test_path)
     test_qcow_path_spec = path_spec_factory.Factory.NewPathSpec(
         definitions.TYPE_INDICATOR_QCOW, parent=test_os_path_spec)
-    self._tsk_partition_path_spec = path_spec_factory.Factory.NewPathSpec(
+    test_tsk_partition_path_spec = path_spec_factory.Factory.NewPathSpec(
         definitions.TYPE_INDICATOR_TSK_PARTITION, location='/p1',
         parent=test_qcow_path_spec)
     self._cs_path_spec = path_spec_factory.Factory.NewPathSpec(
-        definitions.TYPE_INDICATOR_CS, parent=self._tsk_partition_path_spec,
+        definitions.TYPE_INDICATOR_CS, parent=test_tsk_partition_path_spec,
         volume_index=0)
 
     resolver.Resolver.key_chain.SetCredential(
         self._cs_path_spec, 'password', self._FVDE_PASSWORD)
 
-  def testOpenCloseInode(self):
-    """Test the open and close functionality using an inode."""
-    self._TestOpenCloseInode(self._cs_path_spec)
+  def testOpenCloseIdentifier(self):
+    """Test the open and close functionality using an identifier."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestOpenCloseIdentifier(file_object)
 
   def testOpenCloseLocation(self):
     """Test the open and close functionality using a location."""
-    self._TestOpenCloseLocation(self._cs_path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, location='/passwords.txt',
+        parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestOpenCloseLocation(file_object)
 
     # Try open with a path specification that has no parent.
-    path_spec = path_spec_factory.Factory.NewPathSpec(
-        definitions.TYPE_INDICATOR_CS, location='/cs1',
-        parent=self._tsk_partition_path_spec)
     path_spec.parent = None
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
 
     with self.assertRaises(errors.PathSpecError):
-      self._TestOpenCloseLocation(path_spec)
+      self._TestOpenCloseLocation(file_object)
 
   def testSeek(self):
     """Test the seek functionality."""
-    self._TestSeek(self._cs_path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_ANOTHER_FILE,
+        location='/a_directory/another_file', parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestSeek(file_object)
 
   def testRead(self):
     """Test the read functionality."""
-    self._TestRead(self._cs_path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, location='/passwords.txt',
+        parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestRead(file_object)
 
 
-class CSFileWithPathSpecCredentialsTest(test_lib.ImageFileTestCase):
+class CSFileWithPathSpecCredentialsTest(test_lib.HFSImageFileTestCase):
   """Tests the Core Storage (CS) file-like object.
 
   The credentials are passed via the path specification.
   """
   _FVDE_PASSWORD = 'fvde-TEST'
 
-  _INODE_PASSWORDS_TXT = 26
-  _INODE_ANOTHER_FILE = 24
+  _IDENTIFIER_ANOTHER_FILE = 24
+  _IDENTIFIER_PASSWORDS_TXT = 26
 
   def setUp(self):
     """Sets up the needed objects used throughout the test."""
@@ -89,37 +178,58 @@ class CSFileWithPathSpecCredentialsTest(test_lib.ImageFileTestCase):
         definitions.TYPE_INDICATOR_OS, location=test_path)
     test_qcow_path_spec = path_spec_factory.Factory.NewPathSpec(
         definitions.TYPE_INDICATOR_QCOW, parent=test_os_path_spec)
-    self._tsk_partition_path_spec = path_spec_factory.Factory.NewPathSpec(
+    test_tsk_partition_path_spec = path_spec_factory.Factory.NewPathSpec(
         definitions.TYPE_INDICATOR_TSK_PARTITION, location='/p1',
         parent=test_qcow_path_spec)
     self._cs_path_spec = path_spec_factory.Factory.NewPathSpec(
-        definitions.TYPE_INDICATOR_CS, parent=self._tsk_partition_path_spec,
+        definitions.TYPE_INDICATOR_CS, parent=test_tsk_partition_path_spec,
         password=self._FVDE_PASSWORD, volume_index=0)
 
-  def testOpenCloseInode(self):
-    """Test the open and close functionality using an inode."""
-    self._TestOpenCloseInode(self._cs_path_spec)
+  def testOpenCloseIdentifier(self):
+    """Test the open and close functionality using an identifier."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestOpenCloseIdentifier(file_object)
 
   def testOpenCloseLocation(self):
     """Test the open and close functionality using a location."""
-    self._TestOpenCloseLocation(self._cs_path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, location='/passwords.txt',
+        parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestOpenCloseLocation(file_object)
 
     # Try open with a path specification that has no parent.
-    path_spec = path_spec_factory.Factory.NewPathSpec(
-        definitions.TYPE_INDICATOR_CS, location='/cs1',
-        parent=self._tsk_partition_path_spec)
     path_spec.parent = None
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
 
     with self.assertRaises(errors.PathSpecError):
-      self._TestOpenCloseLocation(path_spec)
+      self._TestOpenCloseLocation(file_object)
 
   def testSeek(self):
     """Test the seek functionality."""
-    self._TestSeek(self._cs_path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_ANOTHER_FILE,
+        location='/a_directory/another_file', parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestSeek(file_object)
 
   def testRead(self):
     """Test the read functionality."""
-    self._TestRead(self._cs_path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_HFS,
+        identifier=self._IDENTIFIER_PASSWORDS_TXT, location='/passwords.txt',
+        parent=self._cs_path_spec)
+    file_object = hfs_file_io.HFSFile(self._resolver_context, path_spec)
+
+    self._TestRead(file_object)
 
 
 if __name__ == '__main__':
