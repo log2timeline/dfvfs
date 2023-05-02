555b88624cf459aed51e18a884480bebc8e3562e
Joachim Metz <joachim.metz@gmail.com>
2023-05-02 06:21:21 +0200

    Added BSD disklabel tests for TSK back-end

--- tests/vfs/tsk_partition_file_entry.py
+++ tests/vfs/tsk_partition_file_entry.py
@@ -90,7 +90,16 @@ class TSKPartitionFileEntryTestAPM(shared_test_lib.BaseTestCase):
     parent_file_entry = file_entry.GetParentFileEntry()
     self.assertIsNone(parent_file_entry)
 
-  # TODO: add tests for GetTSKVsPart
+  def testGetTSKVsPart(self):
+    """Tests the GetTSKVsPart function."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=0,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+
+    self.assertIsNotNone(file_entry)
+    tsk_vs_part = file_entry.GetTSKVsPart()
+    self.assertIsNotNone(tsk_vs_part)
 
   def testIsFunctions(self):
     """Test the Is? functions."""
@@ -196,6 +205,196 @@ class TSKPartitionFileEntryTestAPM(shared_test_lib.BaseTestCase):
     self.assertIsNone(data_stream)
 
 
+class TSKPartitionFileEntryTestBSDDisklabel(shared_test_lib.BaseTestCase):
+  """TSK partition file entry tests on BSD disklabel."""
+
+  def setUp(self):
+    """Sets up the needed objects used throughout the test."""
+    self._resolver_context = context.Context()
+    test_path = self._GetTestFilePath(['ufs1.raw'])
+    self._SkipIfPathNotExists(test_path)
+
+    self._os_path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_OS, location=test_path)
+    self._tsk_partition_path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, location='/',
+        parent=self._os_path_spec)
+
+    self._file_system = tsk_partition_file_system.TSKPartitionFileSystem(
+        self._resolver_context, self._tsk_partition_path_spec)
+    self._file_system.Open()
+
+  def tearDown(self):
+    """Cleans up the needed objects used throughout the test."""
+    self._resolver_context.Empty()
+
+  # mmls test_data/ufs1.raw
+  # BSD Disk Label
+  # Offset Sector: 0
+  # Units are in 512-byte sectors
+  #
+  #       Slot      Start        End          Length       Description
+  # 000:  002       0000000000   0000008191   0000008192   Unused (0x00)
+  # 001:  Meta      0000000001   0000000001   0000000001   Partition Table
+  # 002:  000       0000000016   0000008191   0000008176   Unused (0x00)
+
+  def testIntialize(self):
+    """Test the __init__ function."""
+    file_entry = tsk_partition_file_entry.TSKPartitionFileEntry(
+        self._resolver_context, self._file_system,
+        self._tsk_partition_path_spec, is_virtual=True)
+
+    self.assertIsNotNone(file_entry)
+
+  # TODO: add tests for _GetDirectory
+  # TODO: add tests for _GetSubFileEntries
+
+  def testName(self):
+    """Test the name property."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+
+    self.assertIsNotNone(file_entry)
+    self.assertEqual(file_entry.name, 'p2')
+
+  def testSize(self):
+    """Test the size property."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+
+    self.assertIsNotNone(file_entry)
+    self.assertEqual(file_entry.size, 4186112)
+
+  def testGetParentFileEntry(self):
+    """Tests the GetParentFileEntry function."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+
+    self.assertIsNotNone(file_entry)
+    parent_file_entry = file_entry.GetParentFileEntry()
+    self.assertIsNone(parent_file_entry)
+
+  def testGetTSKVsPart(self):
+    """Tests the GetTSKVsPart function."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+
+    self.assertIsNotNone(file_entry)
+    tsk_vs_part = file_entry.GetTSKVsPart()
+    self.assertIsNotNone(tsk_vs_part)
+
+  def testIsFunctions(self):
+    """Test the Is? functions."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+    self.assertIsNotNone(file_entry)
+
+    self.assertFalse(file_entry.IsRoot())
+    self.assertFalse(file_entry.IsVirtual())
+    self.assertTrue(file_entry.IsAllocated())
+
+    self.assertFalse(file_entry.IsDevice())
+    self.assertFalse(file_entry.IsDirectory())
+    self.assertTrue(file_entry.IsFile())
+    self.assertFalse(file_entry.IsLink())
+    self.assertFalse(file_entry.IsPipe())
+    self.assertFalse(file_entry.IsSocket())
+
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, location='/',
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+    self.assertIsNotNone(file_entry)
+
+    self.assertTrue(file_entry.IsRoot())
+    self.assertTrue(file_entry.IsVirtual())
+    self.assertTrue(file_entry.IsAllocated())
+
+    self.assertFalse(file_entry.IsDevice())
+    self.assertTrue(file_entry.IsDirectory())
+    self.assertFalse(file_entry.IsFile())
+    self.assertFalse(file_entry.IsLink())
+    self.assertFalse(file_entry.IsPipe())
+    self.assertFalse(file_entry.IsSocket())
+
+  def testSubFileEntries(self):
+    """Test the sub file entries iteration functionality."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, location='/',
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+    self.assertIsNotNone(file_entry)
+
+    self.assertEqual(file_entry.number_of_sub_file_entries, 3)
+
+    expected_sub_file_entry_names = ['p1', '', 'p2']
+
+    sub_file_entry_names = []
+    for sub_file_entry in file_entry.sub_file_entries:
+      sub_file_entry_names.append(sub_file_entry.name)
+
+    self.assertEqual(
+        len(sub_file_entry_names), len(expected_sub_file_entry_names))
+    self.assertEqual(
+        sorted(sub_file_entry_names), sorted(expected_sub_file_entry_names))
+
+  def testDataStreams(self):
+    """Test the data streams functionality."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+    self.assertIsNotNone(file_entry)
+
+    self.assertEqual(file_entry.number_of_data_streams, 1)
+
+    data_stream_names = []
+    for data_stream in file_entry.data_streams:
+      data_stream_names.append(data_stream.name)
+
+    self.assertEqual(data_stream_names, [''])
+
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, location='/',
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+    self.assertIsNotNone(file_entry)
+
+    self.assertEqual(file_entry.number_of_data_streams, 0)
+
+    data_stream_names = []
+    for data_stream in file_entry.data_streams:
+      data_stream_names.append(data_stream.name)
+
+    self.assertEqual(data_stream_names, [])
+
+  def testGetDataStream(self):
+    """Tests the GetDataStream function."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=2,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+    self.assertIsNotNone(file_entry)
+
+    data_stream_name = ''
+    data_stream = file_entry.GetDataStream(data_stream_name)
+    self.assertIsNotNone(data_stream)
+    self.assertEqual(data_stream.name, data_stream_name)
+
+    data_stream = file_entry.GetDataStream('bogus')
+    self.assertIsNone(data_stream)
+
+
 class TSKPartitionFileEntryTestMBR(shared_test_lib.BaseTestCase):
   """TSK partition file entry tests on MBR."""
 
@@ -276,7 +475,16 @@ class TSKPartitionFileEntryTestMBR(shared_test_lib.BaseTestCase):
     parent_file_entry = file_entry.GetParentFileEntry()
     self.assertIsNone(parent_file_entry)
 
-  # TODO: add tests for GetTSKVsPart
+  def testGetTSKVsPart(self):
+    """Tests the GetTSKVsPart function."""
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        definitions.TYPE_INDICATOR_TSK_PARTITION, part_index=1,
+        parent=self._os_path_spec)
+    file_entry = self._file_system.GetFileEntryByPathSpec(path_spec)
+
+    self.assertIsNotNone(file_entry)
+    tsk_vs_part = file_entry.GetTSKVsPart()
+    self.assertIsNotNone(tsk_vs_part)
 
   def testIsFunctions(self):
     """Test the Is? functions."""
