c2a71e6c30771a1807c4c85871a2a8008e494cbe
Joachim Metz <joachim.metz@gmail.com>
2025-11-25 13:10:10 +0100

    Migrated setup.cfg to pyproject.toml

--- utils/update_release.sh
+++ utils/update_release.sh
@@ -3,16 +3,16 @@
 # Script that makes changes in preparation of a new release, such as updating
 # the version and documentation.
 
-EXIT_FAILURE=1;
-EXIT_SUCCESS=0;
+EXIT_FAILURE=1
+EXIT_SUCCESS=0
 
 VERSION=$(date -u +"%Y%m%d")
 
 # Update the Python module version.
 sed "s/__version__ = '[0-9]*'/__version__ = '${VERSION}'/" -i dfvfs/__init__.py
 
-# Update the version in the setuptools configuration.
-sed "s/version = [0-9]*/version = ${VERSION}/" -i setup.cfg
+# Update the version in the pyproject configuration.
+sed "s/version = \"[0-9]*\"/version = \"${VERSION}\"/" -i pyproject.toml
 
 # Ensure shebangs of Python scripts are consistent.
 find . -name \*.py -exec sed '1s?^#!.*$?#!/usr/bin/env python3?' -i {} \;
@@ -35,5 +35,5 @@ EOT
 # Regenerate the API documentation.
 tox -edocs
 
-exit ${EXIT_SUCCESS};
+exit ${EXIT_SUCCESS}
 
